ed416fcacd879a2442b621822ec55789e6e7e119
Yegor Yefremov <yegorslists@googlemail.com>
2012-11-28 11:15:18 +0100

    net: add ICPlus PHY driver
    The driver code was taken from Linux kernel source:
    drivers/net/phy/icplus.c
    
    Signed-off-by: Zoltan HERPAI <wigyori@uid0.hu>
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>

--- include/phy.h
+++ include/phy.h
@@ -325,6 +325,7 @@ int phy_cortina_init(void);
 int phy_cortina_access_init(void);
 int phy_davicom_init(void);
 int phy_et1011c_init(void);
+int phy_icplus_init(void);
 int phy_lxt_init(void);
 int phy_marvell_init(void);
 int phy_micrel_ksz8xxx_init(void);
